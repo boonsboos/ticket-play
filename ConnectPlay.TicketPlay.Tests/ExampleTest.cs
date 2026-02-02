a046735ef9eab453429f05be50cdc2051612ba33
boonsboos <boons@boonsboos.nl>
2026-02-02 22:28:06 +0100

    feat: introduce project and vendor bootstrap

---
+++ ConnectPlay.TicketPlay.Tests/ExampleTest.cs
@@ -0,0 +1,11 @@
+﻿namespace ConnectPlay.TicketPlay.Tests;
+
+[TestClass]
+public sealed class ExampleTest
+{
+    [TestMethod]
+    public void ExampleTestMethod()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
---
+++ ConnectPlay.TicketPlay/ExampleClass.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// Use this module for creating classes/models that are common (shared) between the API and the UI.
+/// </summary>
+namespace ConnectPlay.TicketPlay;
+
+public class ExampleClass
+{
+
+}
