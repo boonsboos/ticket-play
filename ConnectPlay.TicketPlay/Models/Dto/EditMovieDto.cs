ad13abbdb16a38bbb7d104baacc9351ac2511b35
boonsboos <boons@boonsboos.nl>
2026-02-19 17:06:41 +0100

    add files

---
+++ ConnectPlay.TicketPlay/Models/Dto/EditMovieDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectPlay.TicketPlay.Models.Dto;
+
+public record EditMovieDto
+{
+    public string? Description { get; init; }
+    public string? Tags { get; init; }
+    public int? Age { get; init; }
+    public string? PosterUrl { get; init; }
+}
