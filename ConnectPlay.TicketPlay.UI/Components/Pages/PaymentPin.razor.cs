fc39653bfad994d5d0c56e3906075e652880cd73
mastermooss <mastersmoos@gmail.com>
2026-03-06 22:07:03 +0100

    Added the Pin page and Success Page for checkout of tickets

---
+++ ConnectPlay.TicketPlay.UI/Components/Pages/PaymentPin.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using NSubstitute.Core;
+
+namespace ConnectPlay.TicketPlay.UI.Components.Pages;
+
+public partial class PaymentPin
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+    public int[] PinNumbers { get; } = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    public string EnteredPin { get; set; } = string.Empty;
+    public string Title { get; set; } = "Voer uw pincode in.";
+
+    public void AddPinNumber(int number)
+    {
+        if (EnteredPin.Length < 4)
+        {
+            EnteredPin += number;
+        }
+    }
+
+    // TODO(): Call the api to release the seats an cancel order
+    public void CancelPayment()
+    {
+        EnteredPin = string.Empty;
+        NavigationManager.NavigateTo("/");
+    }
+
+    public void RemoveLastNumber()
+    {
+        if(EnteredPin.Length > 0)
+        {
+            EnteredPin = EnteredPin.Substring(0, EnteredPin.Length - 1);
+        }
+    }
+
+    public void PaymentSuccessful()
+    {
+        if (EnteredPin == "1234" || EnteredPin == "9874")
+        {
+            NavigationManager.NavigateTo("/payment/success");
+        }
+        else
+        {
+            EnteredPin = string.Empty;
+            Title = "Ongeldige pincode.";
+        }
+    }
+}
