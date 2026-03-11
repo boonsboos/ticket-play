123559a35a78c632f6280d6ce9679c6bbc667b70
mastermooss <60898036+mastermooss@users.noreply.github.com>
2026-03-11 14:12:19 +0100

    [FDN-11] Added the Pin page and Success Page for checkout of tickets (#9)

--- ConnectPlay.TicketPlay.UI/Api/IKioskApi.cs
+++ ConnectPlay.TicketPlay.UI/Api/IKioskApi.cs
@@ -7,4 +7,10 @@ public interface IKioskApi
 {
     [Post("/kiosk/{id}/reserve")]
     Task<ApiResponse<Order>> ReserveSeatsAsync([AliasAs("id")] int screeningId, IEnumerable<TicketType> tickets);
+    
+    [Put("/kiosk/{id}/cancel")] // use PUT to update the order status if the order is canceled
+    Task<ApiResponse<object>> CancelOrderAsync([AliasAs("id")] int orderId);
+
+    [Put("/kiosk/{id}/pay")]
+    Task<ApiResponse<object>> PayOrderAsync([AliasAs("id")] int orderId);
 }
