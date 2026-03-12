126a31a79fe317d3fe4b42c062f0f6d6080e7c85
boonsboos <mrsherobrinenaomi@gmail.com>
2026-03-12 13:06:32 +0100

    [FDN-12] thuis bestelde kaartjes uitprinten

---
+++ ConnectPlay.TicketPlay.UI/Api/IOrderApi.cs
@@ -0,0 +1,10 @@
+﻿using ConnectPlay.TicketPlay.Models;
+using Refit;
+
+namespace ConnectPlay.TicketPlay.UI.Api;
+
+public interface IOrderApi
+{
+    [Get("/kiosk/{orderCode}")]
+    public Task<ApiResponse<Order>> GetOrderByOrderCodeAsync(string orderCode);
+}
--- ConnectPlay.TicketPlay/Abstract/Repositories/IOrderRepository.cs
+++ ConnectPlay.TicketPlay/Abstract/Repositories/IOrderRepository.cs
@@ -7,4 +7,5 @@ public interface IOrderRepository
     public Task<Order> CreateOrderAsync(Order order);
     public Task<Order?> GetOrderByIdAsync(int orderId);
     public Task UpdateOrderStatusAsync(int orderId, OrderStatus status);
+    public Task<Order?> GetOrderByOrderCodeAsync(string orderCode);
 }
