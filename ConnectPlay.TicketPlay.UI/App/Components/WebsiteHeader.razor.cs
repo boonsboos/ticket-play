96ea060b4dc0d20e832b1c08396a2934de421cbf
mastermooss <60898036+mastermooss@users.noreply.github.com>
2026-03-14 13:26:09 +0100

    [FDN-57] - Added component Header and Footer (#22)

---
+++ ConnectPlay.TicketPlay.UI/App/Components/WebsiteHeader.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+
+namespace ConnectPlay.TicketPlay.UI.App.Components;
+
+public partial class WebsiteHeader : ComponentBase // ComponentBase is the baseclass for a Blazor component
+{
+    // We use protected so the WebsiteHeader.razor component can access this property
+    protected string Language { get; set; } = "NL";
+
+    protected void ToggleLanguage()
+    {
+        if (Language == "NL")
+        {
+            Language = "ENG";
+        }
+        else
+        {
+            Language = "NL";
+        }
+    }
+}
