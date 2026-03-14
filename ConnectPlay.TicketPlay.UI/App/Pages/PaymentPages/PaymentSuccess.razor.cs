854ab4a7641ddf54749950dc54f4c96704d2d6ee
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2026-03-14 15:08:31 +0100

    Added PaymentPin & PaymentSuccess Pages to website and makes use of website service

---
+++ ConnectPlay.TicketPlay.UI/App/Pages/PaymentPages/PaymentSuccess.razor.cs
@@ -0,0 +1,23 @@
+﻿using ConnectPlay.TicketPlay.UI.Configuration;
+using ConnectPlay.TicketPlay.UI.Services;
+using Microsoft.Extensions.Options;
+
+namespace ConnectPlay.TicketPlay.UI.App.Pages.PaymentPages;
+
+public partial class PaymentSuccess
+{
+    private readonly ApiConfiguration options;
+
+    private readonly WebsiteService websiteService;
+
+    public PaymentSuccess(WebsiteService websiteService, IOptions<ApiConfiguration> options)
+    {
+        this.websiteService = websiteService;
+        this.options = options.Value;
+    }
+
+    private string GetTicketFileUrl()
+    {
+        return options.BaseUrl + $"/{websiteService.CurrentOrderId}/pdf";
+    }
+}
