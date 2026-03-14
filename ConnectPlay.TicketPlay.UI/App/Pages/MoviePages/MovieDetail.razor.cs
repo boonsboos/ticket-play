f0782351dcca89217602f2867852f7f9c617dd48
boonsboos <boons@boonsboos.nl>
2026-03-14 13:30:54 +0100

    [FDN-17] migrate kiosk movie detail page to website

---
+++ ConnectPlay.TicketPlay.UI/App/Pages/MoviePages/MovieDetail.razor.cs
@@ -0,0 +1,72 @@
+﻿using ConnectPlay.TicketPlay.Abstract.Repositories;
+using ConnectPlay.TicketPlay.Contracts.Movie;
+using ConnectPlay.TicketPlay.Models;
+using ConnectPlay.TicketPlay.UI.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace ConnectPlay.TicketPlay.UI.App.Pages.MoviePages;
+
+public partial class MovieDetail : ComponentBase
+{
+    [Parameter] public int Id { get; set; }
+
+    private readonly WebsiteService websiteService;
+    private readonly IMovieRepository movieRepository;
+    private readonly IScreeningRepository screeningRepository;
+    private readonly NavigationManager navigationManager;
+    private readonly ILogger<MovieDetail> logger;
+
+    private MovieDetailResponse? movie;
+    private IEnumerable<Screening> screenings = [];
+    private int? _loadedId;
+
+    public MovieDetail(WebsiteService websiteService, IMovieRepository movieRepository, IScreeningRepository screeningRepository, NavigationManager navigationManager, ILogger<MovieDetail> logger)
+    {
+        this.websiteService = websiteService;
+        this.movieRepository = movieRepository;
+        this.screeningRepository = screeningRepository;
+        this.navigationManager = navigationManager;
+        this.logger = logger;
+    }
+
+
+    protected override async Task OnParametersSetAsync()
+    {
+
+        if (Id == 0 || _loadedId == Id)
+        {
+            return;
+        }
+
+        _loadedId = Id;
+
+        try
+        {
+            movie = await movieRepository.GetMovieByIdAsync(Id);
+
+            screenings = await screeningRepository.GetTodayScreeningsFromMovieAsync(Id);
+        }
+        catch (Exception ex)
+        {
+            movie = null;
+            screenings = [];
+            logger.LogError(ex, "Error fetching movie with Id {MovieId}", Id);
+        }
+    }
+
+    public void SetSelectedScreening(Screening screening)
+    {
+        // Toggle selection if the same screening is clicked again
+        if (websiteService.SelectedScreening?.Id == screening.Id)
+        {
+            websiteService.SelectedScreening = null;
+            return;
+        }
+        // Only allow selection of screenings that haven't started yet
+        if (screening.StartTime >= DateTime.Now)
+            websiteService.SelectedScreening = screening;
+    }
+
+    protected void Back() => navigationManager.NavigateTo("/movies");
+    protected void ToTickets() => navigationManager.NavigateTo("/tickets");
+}
---
+++ ConnectPlay.TicketPlay.UI/Services/WebsiteService.cs
@@ -0,0 +1,111 @@
+﻿using ConnectPlay.TicketPlay.Models;
+using ConnectPlay.TicketPlay.UI.Api;
+
+namespace ConnectPlay.TicketPlay.UI.Services;
+
+public class WebsiteService
+{
+    private readonly IKioskApi kioskApi;
+    private readonly ILogger<KioskService> logger;
+
+    private Order? currentOrder = null;
+
+    public IEnumerable<Seat> Seats { get { return currentOrder?.Tickets.Select(ticket => ticket.Seat) ?? []; } }
+
+    public Movie? Movie => currentOrder?.Tickets.First()?.Screening.Movie;
+    public int? CurrentOrderId { get => currentOrder?.Id; } // Only get the order id if there is a current order
+    public Screening? SelectedScreening { get; set; } = null;
+    public IEnumerable<TicketType> Tickets { get; set; } = [];
+
+    public WebsiteService(IKioskApi kioskApi, ILogger<KioskService> logger)
+    {
+        this.kioskApi = kioskApi;
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Called after tickets have been selected
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public async Task PlaceOrder()
+    {
+        ArgumentNullException.ThrowIfNull(SelectedScreening, nameof(SelectedScreening));
+        if (!Tickets.Any()) throw new ArgumentException("Tickets cannot be empty");
+
+        var response = await kioskApi.ReserveSeatsAsync(SelectedScreening.Id, Tickets);
+        if (response.IsSuccessStatusCode)
+        {
+            currentOrder = response.Content;
+        }
+        else
+        {
+            logger.LogError("Received {Response} from API: {Error}", response.StatusCode, response.Error);
+        }
+    }
+
+    public async Task CancelOrder()
+    {
+        // Ensure that there is a current order id to cancel else trow exception
+        var orderId = CurrentOrderId ?? throw new ArgumentNullException(nameof(CurrentOrderId));
+
+        // kioskApi is the API Client injected into the service
+        // CancelOrderAsync() send a resuqest to the API to cancel the order
+        // With await we wait for the respone frome the api
+        var cancelResponse = await kioskApi.CancelOrderAsync(orderId);
+
+        if (cancelResponse.IsSuccessStatusCode)
+        {
+            Cleanup(); // clean the kioskservice 
+        }
+        else
+        {
+            logger.LogError("Canceling order faild {OrderId}", orderId);
+        }
+    }
+
+    public async Task PayOrder()
+    {
+        var orderId = CurrentOrderId ?? throw new ArgumentNullException(nameof(CurrentOrderId));
+
+        var payResponse = await kioskApi.PayOrderAsync(orderId);
+
+
+        // if the response is not OK (200)
+        if (!payResponse.IsSuccessStatusCode)
+        {
+            logger.LogError("Paying the order faild {OrderId}", orderId);
+        }
+    }
+
+    /// <summary>
+    /// Call after payment is finished and the tickets can be printed
+    /// </summary>
+    /// <returns></returns>
+    public async Task PrintTickets()
+    {
+        Cleanup();
+    }
+
+    private void Cleanup()
+    {
+        logger.LogInformation("Resetting KioskService for use in next order");
+        SelectedScreening = null;
+        Tickets = [];
+        currentOrder = null;
+    }
+
+    public float GetPrice(TicketType ticketType)
+    {
+        var movie = Movie ?? SelectedScreening?.Movie;
+        if (movie == null) return 0;
+        // everyone except regular gets €1,50 off
+        return ticketType switch
+        {
+            TicketType.Regular => RegularPrice(Movie ?? SelectedScreening?.Movie),
+            _ => RegularPrice(Movie ?? SelectedScreening?.Movie) - 1.5f,
+        };
+    }
+
+    private static float RegularPrice(Movie? movie) => (movie?.Duration ?? 90) > 120 ? 9.00f : 8.50f;
+}
