6955590cf87ef4183fec560a21d6f8034d22bbf6
mastermooss <60898036+mastermooss@users.noreply.github.com>
2026-03-20 20:29:03 +0100

    Change TranslationService registration to scoped
    Changed TranslationService from singleton to scoped.

--- ConnectPlay.TicketPlay.UI/Program.cs
+++ ConnectPlay.TicketPlay.UI/Program.cs
@@ -73,7 +73,7 @@ public class Program
         services.AddScoped<IScreeningRepository, ScreeningRepository>();
         services.AddScoped<KioskService>();
         services.AddScoped<WebsiteService>();
-        services.AddSingleton<TranslationService>(); // We use singelton because we want only one instance of the translation service
+        services.AddScoped<TranslationService>();
     }
 
     private static void ConfigureApi(IServiceCollection services, string baseUrl)
