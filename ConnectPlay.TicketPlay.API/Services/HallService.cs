fc9b082f47336e3509d66872daa8aaf41eeb69a4
Mingull <nielsplug@outlook.com>
2026-03-22 12:49:29 +0100

    Fixed typo and added comments to GetHallLayout

--- ConnectPlay.TicketPlay.API/Services/HallService.cs
+++ ConnectPlay.TicketPlay.API/Services/HallService.cs
@@ -6,6 +6,13 @@ namespace ConnectPlay.TicketPlay.API.Services;
 
 public class HallOrderService(IHallRepository hallRepository, ILogger<HallOrderService> logger) : IHallService
 {
+    /// <summary>
+    /// Gets the layout of a hall by its ID.
+    /// <para>The layout is represented as a list of integers, where each integer represents the number of seats in a row.</para>
+    /// <para>The wheelchair seat is also included in the response, indicating its row and seat number.</para>
+    /// </summary>
+    /// <param name="hallId"></param>
+    /// <returns></returns>
     public async Task<HallLayoutResponse?> GetHallLayoutAsync(int hallId)
     {
         var hall = await hallRepository.GetHallByIdAsync(hallId);
@@ -13,20 +20,20 @@ public class HallOrderService(IHallRepository hallRepository, ILogger<HallOrderS
         if (hall is null)
             return null;
 
+        // Group the seats by row and count the number of seats in each row to create the layout.
         var layout = hall.Seats
             .GroupBy(s => s.Row)
             .OrderBy(g => g.Key)
             .Select(g => g.Count())
             .ToList();
 
+        // Find the wheelchair seat in the hall and create a response object with the layout and wheelchair seat information.
         var hallLayout = new HallLayoutResponse
         {
             Layout = layout,
-            WeelchairSeat = new(hall.Seats.FirstOrDefault(s => s.IsForWheelchair)?.Row ?? 0, hall.Seats.FirstOrDefault(s => s.IsForWheelchair)?.SeatNumber ?? 0),
+            WheelchairSeat = new(hall.Seats.FirstOrDefault(s => s.IsForWheelchair)?.Row ?? 0, hall.Seats.FirstOrDefault(s => s.IsForWheelchair)?.SeatNumber ?? 0),
         };
 
-        var json = System.Text.Json.JsonSerializer.Serialize(hallLayout);
-        logger.LogInformation("Layout = {Layout}", json);
         return hallLayout;
     }
 }
--- ConnectPlay.TicketPlay/Contracts/Hall/HallLayoutResponse.cs
+++ ConnectPlay.TicketPlay/Contracts/Hall/HallLayoutResponse.cs
@@ -3,5 +3,5 @@ namespace ConnectPlay.TicketPlay.Contracts.Hall;
 public sealed record HallLayoutResponse
 {
     public required List<int> Layout { get; init; } = [];
-    public required HallWheelchairSeat WeelchairSeat { get; init; }
+    public required HallWheelchairSeat WheelchairSeat { get; init; }
 }
