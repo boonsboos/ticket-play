2261592ffabfddfe90c4f24e98926dd78342560e
Tippe van Roosmalen <tippevanroosmalen@outlook.com>
2026-03-23 19:22:23 +0100

    Updated the abstract & created contract for sending newsletters

---
+++ ConnectPlay.TicketPlay/Contracts/Newsletter/NewsletterRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectPlay.TicketPlay.Contracts.Newsletter;
+
+public record NewsletterRequest
+{
+    [StringLength(255)]
+    public required string Topic { get; init; }
+
+    [StringLength(4000)]
+    public required string Content { get; init; }
+}
