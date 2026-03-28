745b1745a5e8d40b7784db025107e8762e8a8210
boonsboos <boons@boonsboos.nl>
2026-03-28 16:30:07 +0100

    add orderarrangmentrepository

---
+++ ConnectPlay.TicketPlay.API/Repositories/OrderArrangementRepository.cs
@@ -0,0 +1,43 @@
+﻿using ConnectPlay.TicketPlay.Abstract.Repositories;
+using ConnectPlay.TicketPlay.API.Contexts;
+using ConnectPlay.TicketPlay.Contracts.Arrangements;
+using ConnectPlay.TicketPlay.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlay.TicketPlay.API.Repositories;
+
+public class OrderArrangementRepository : IOrderArrangementRepository
+{
+    private readonly IDbContextFactory<TicketPlayContext> dbContextFactory;
+
+    public OrderArrangementRepository(IDbContextFactory<TicketPlayContext> dbContextFactory)
+    {
+        this.dbContextFactory = dbContextFactory;
+    }
+
+    public async Task<OrderArrangement> SaveArrangementAsync(Order order, ArrangementQuantity quantity)
+    {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+
+        var arrangement = new OrderArrangement { Amount = quantity.Quantity, Arrangement = quantity.Arrangement, Order = order };
+
+        dbContext.Add(arrangement);
+
+        await dbContext.SaveChangesAsync();
+
+        return arrangement;
+    }
+
+    public async Task<IEnumerable<OrderArrangement>> SaveArrangementsAsync(Order order, IEnumerable<ArrangementQuantity> quantities)
+    {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+
+        var arrangements = quantities.Select(quantity => new OrderArrangement { Amount = quantity.Quantity, Arrangement = quantity.Arrangement, Order = order });
+
+        dbContext.AddRange(arrangements);
+
+        await dbContext.SaveChangesAsync();
+
+        return arrangements;
+    }
+}
---
+++ ConnectPlay.TicketPlay/Abstract/Repositories/IOrderArrangementRepository.cs
@@ -0,0 +1,10 @@
+﻿using ConnectPlay.TicketPlay.Contracts.Arrangements;
+using ConnectPlay.TicketPlay.Models;
+
+namespace ConnectPlay.TicketPlay.Abstract.Repositories;
+
+public interface IOrderArrangementRepository
+{
+    public Task<IEnumerable<OrderArrangement>> SaveArrangementsAsync(Order order, IEnumerable<ArrangementQuantity> quantities);
+    public Task<OrderArrangement> SaveArrangementAsync(Order order, ArrangementQuantity quantity);
+}
