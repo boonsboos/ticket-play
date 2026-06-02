7c5116bf2d661ed7b6c66c832d990414ab2b24da
boonsboos <boons@boonsboos.nl>
2026-06-02 23:20:16 +0200

    [ATP-4] dep: add jwt decoding + missing file

---
+++ ConnectPlay.TicketPlay/Abstract/Repositories/IFavoritesRepository.cs
@@ -0,0 +1,10 @@
+﻿using ConnectPlay.TicketPlay.Models;
+
+namespace ConnectPlay.TicketPlay.Abstract.Repositories;
+
+public interface IFavoritesRepository
+{
+    Task AddFavoriteAsync(Guid userId, int movieId);
+    Task RemoveFavoriteAsync(Guid userId, int movieId);
+    Task<IEnumerable<Movie>> GetFavoritesAsync(Guid userId);
+}
